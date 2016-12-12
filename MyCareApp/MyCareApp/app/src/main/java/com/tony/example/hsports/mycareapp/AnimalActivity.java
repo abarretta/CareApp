cf8424efe9f96b3aa7f11e9c1b8086888ba523bc
J Walker Holland <walker@retrodev.org>
2016-12-11 22:33:51 -0500

    Update AnimalActivity to load animals from list

--- MyCareApp/MyCareApp/app/src/main/java/com/tony/example/hsports/mycareapp/AnimalActivity.java
+++ MyCareApp/MyCareApp/app/src/main/java/com/tony/example/hsports/mycareapp/AnimalActivity.java
@@ -1,12 +1,13 @@
 package com.tony.example.hsports.mycareapp;
 
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.content.Intent;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 /**
  * Created by Tony on 11/24/2016.
@@ -17,6 +18,15 @@ public class AnimalActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.animal_layout);
+
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            // landscape
+            linearlayout.setOrientation(LinearLayout.HORIZONTAL);
+        }
+        else {
+            // portrait
+            linearlayout.setOrientation(LinearLayout.VERTICAL);
+        }
 
         ImageButton luciousButton = (ImageButton)  findViewById(R.id.luciousButton);
         luciousButton.setOnClickListener(new View.OnClickListener(){
